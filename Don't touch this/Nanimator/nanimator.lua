e4dd4f6a10baf28130b88b0e5fc32001e7202426
Nanskip <nanskip.oof@gmail.com>
2024-06-21 18:10:58 +0300

    Revert "some features"
    This reverts commit cd64b47732128b9c1ce4f3bffe3583f9956a6083.

--- Don't touch this/Nanimator/nanimator.lua
+++ Don't touch this/Nanimator/nanimator.lua
@@ -14,7 +14,6 @@ Client.OnStart = function()
     Camera:SetModeFree()
     Camera.FOV = 60
     globalDx = 0 globalDy = 0
-    scrollNumber = 1
     --Player:SetParent(nil)
 
     createUI()
@@ -53,6 +52,8 @@ Pointer.Down = function( pointerEvent )
     else
         selectedObject = impact.Object
         gizmo:setObject(selectedObject)
+
+        print(selectedObject)
     end
 end
 
@@ -115,13 +116,14 @@ createUI = function()
         local items_gallery = gallery:create(maxModalWidth, maxModalHeight, updateModalPosition)
         local grid = items_gallery.contentStack[1].node
         grid.onOpen = function(self, cell)
+            print("Model name: " .. cell.itemFullName)
+
             Object:Load(cell.itemFullName, function(self)
                 if model ~= nil then
                     model:SetParent(nil)
                 end
                 model = self
                 model:SetParent(World)
-                loadModel(model)
             end)
             items_gallery:close()
         end
@@ -129,166 +131,13 @@ createUI = function()
 
     timeline = Object()
     timeline.background = ui:createFrame(Color(0, 0, 0, 0.5))
-    timeline.background.size = Number2(Screen.Width - 40 - 200, 210)
+    timeline.background.size = Number2(Screen.Width - 40 - 200, 200)
     timeline.background.pos = Number2(10, 10)
     timeline.background.onPress = function() return end
-
-    timeline.background2 = ui:createFrame(Color(0, 0, 0, 0.3))
-    timeline.background2.size = Number2(255, 210)
-    timeline.background2.pos = Number2(10, 10)
-
-    timeline.backgroundVertLine = ui:createFrame(Color(0, 0, 0, 0.2))
-    timeline.backgroundVertLine.size = Number2(5, timeline.background.size.Y)
-    timeline.backgroundVertLine.pos = Number2(265, 10)
-    timeline.backgroundVertLine2 = ui:createFrame(Color(0, 0, 0, 0.2))
-    timeline.backgroundVertLine2.size = Number2(5, timeline.background.size.Y)
-    timeline.backgroundVertLine2.pos = Number2(10, 10)
-
-    timeline.shapes = {}
-    timeline.buttons = {}
-
-    timeline.upButton = ui:createButton("⬆")
-    timeline.upButton.pos = Number2(Screen.Width - 200-30-36, 10+210-36)
-    timeline.upButton.onRelease = function()
-        if scrollNumber > 1 then
-            scrollNumber = scrollNumber - 1
-        end
-        timeline.update()
-    end
-
-    timeline.downButton = ui:createButton("⬇")
-    timeline.downButton.pos = Number2(Screen.Width - 200-30-36, 10)
-    timeline.downButton.onRelease = function()
-        if scrollNumber < #timeline.shapes-4 then
-            scrollNumber = scrollNumber + 1
-        end
-        timeline.update()
-    end
-
-    timeline.buttonsBackground = ui:createFrame(Color(0, 0, 0, 0.4))
-    timeline.buttonsBackground.size = Number2(36, 210-(36*2))
-    timeline.buttonsBackground.pos = Number2(Screen.Width - 200-30-36, 46)
-
-    timeline.horizontalLines = {}
-
-    for i=1, 6 do
-        timeline.horizontalLines[i] = ui:createFrame(Color(0, 0, 0, 0.2))
-        timeline.horizontalLines[i].size = Number2(timeline.background.Width-36, 5)
-        timeline.horizontalLines[i].pos = Number2(10, 10 + (41*(i-1)))
-    end
-
-    timeline.update = function()
-        for k, v in ipairs(timeline.buttons) do
-            timeline.buttons[k].pos = Number2(-1000, -1000)
-            timeline.buttons[k].line.pos = Number2(-1000, -1000)
-            timeline.buttons[k].line2.pos = Number2(-1000, -1000)
-            if timeline.buttons[k].line3 ~= nil then
-                timeline.buttons[k].line3.pos = Number2(-1000, -1000)
-            end
-        end
-
-        for i = scrollNumber, scrollNumber + 4 do
-            if timeline.shapes[i] == nil then break end
-
-            local v = timeline.shapes[i]
-
-            timeline.buttons[i].pos = Number2(15, 179 - ((36 + 5))*(i-scrollNumber))
-            timeline.buttons[i].Width = 250
-
-            timeline.buttons[i].pos.X = timeline.buttons[i].pos.X + 10*v.depth
-            timeline.buttons[i].Width = timeline.buttons[i].Width - 10*v.depth
-            timeline.buttons[i].line.size = Number2(math.min(10*v.depth, 10), 5)
-            timeline.buttons[i].line.pos = Number2(timeline.buttons[i].pos.X - 10, timeline.buttons[i].pos.Y + 20-3)
-            timeline.buttons[i].line2.size = Number2(5, 14)
-            timeline.buttons[i].line2.pos = Number2(timeline.buttons[i].pos.X - 10, timeline.buttons[i].pos.Y + 22)
-            if timeline.buttons[i].line3 ~= nil then
-                timeline.buttons[i].line3.size = Number2(5, 17)
-                timeline.buttons[i].line3.pos = Number2(timeline.buttons[i].pos.X - 10, timeline.buttons[i].pos.Y)
-            end
-        end
-    end
 
     animationSelector = Object()
     animationSelector = ui:createFrame(Color(0, 0, 0, 0.5))
     animationSelector.size = Number2(260, 320)
-    animationSelector.pos = Number2(10, 230)
+    animationSelector.pos = Number2(10, 220)
     animationSelector.onPress = function() return end
-end
-
-loadModel = function(model)
-    if model == nil then
-        return
-    end
-
-    scrollNumber = 1
-
-    for k, v in ipairs(timeline.shapes) do
-        timeline.shapes[k]:SetParent(nil)
-        timeline.shapes[k] = nil
-    end
-
-    for k, v in ipairs(timeline.buttons) do
-        timeline.buttons[k].line:remove()
-        timeline.buttons[k].line = nil
-        timeline.buttons[k].line2:remove()
-        timeline.buttons[k].line2 = nil
-
-        if timeline.buttons[k].line3 ~= nil then
-            timeline.buttons[k].line3:remove()
-            timeline.buttons[k].line3 = nil
-        end
-
-        timeline.buttons[k]:remove()
-        timeline.buttons[k] = nil
-    end
-
-    hierarchyActions:applyToDescendants(model,  { includeRoot = true }, function(s)
-        table.insert(timeline.shapes, s)
-        
-        local parent = s:GetParent()
-        if parent.depth == nil then parent.depth = -1 end
-        s.depth = parent.depth + 1
-        s.Physics = PhysicsMode.StaticPerBlock
-    end)
-
-    for k, v in ipairs(timeline.shapes) do
-        local name = v.Name
-        if name == nil or name == "(null)" then name = "Shape " .. math.random(1000, 9999) end
-        timeline.buttons[k] = ui:createButton(name, {borders = false, color = Color(0.2, 0.2, 0.2, 0.3), colorPressed = Color(0.3, 0.3, 0.3, 0.3), shadow = false})
-        timeline.buttons[k].pos = Number2(15, 15 + ((timeline.buttons[k].Height + 5))*(k-1))
-
-        if v.depth == nil then v.depth = 0 end
-        timeline.buttons[k].Width = 250
-
-        timeline.buttons[k].pos.X = timeline.buttons[k].pos.X + 10*v.depth
-        timeline.buttons[k].Width = timeline.buttons[k].Width - 10*v.depth
-        timeline.buttons[k].Height = 36
-        timeline.buttons[k].line = ui:createFrame(Color(255, 255, 255, 0.2))
-        timeline.buttons[k].line.size = Number2(math.min(10*v.depth, 10), 5)
-        timeline.buttons[k].line.pos = Number2(timeline.buttons[k].pos.X - 10, timeline.buttons[k].pos.Y + 20-3)
-
-        timeline.buttons[k].line2 = ui:createFrame(Color(255, 255, 255, 0.2))
-        timeline.buttons[k].line2.size = Number2(5, 14)
-        timeline.buttons[k].line2.pos = Number2(timeline.buttons[k].pos.X - 10, timeline.buttons[k].pos.Y + 22)
-
-        local parent = v:GetParent()
-
-        for i = 1, parent.ChildrenCount do
-            local child = parent:GetChild(i)
-
-            if child == v and i < parent.ChildrenCount then
-                timeline.buttons[k].line3 = ui:createFrame(Color(255, 255, 255, 0.2))
-                timeline.buttons[k].line3.size = Number2(5, 17)
-                timeline.buttons[k].line3.pos = Number2(timeline.buttons[k].pos.X - 10, timeline.buttons[k].pos.Y)
-            end
-        end
-        
-        timeline.buttons[k].onRelease = function()
-
-            selectedObject = v
-            gizmo:setObject(v)
-        end 
-    end
-
-    timeline.update()
 end
